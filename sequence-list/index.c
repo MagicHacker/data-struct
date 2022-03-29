8546b2ab4481c5218cb9c762a32cf46c15b1a4e6
xuan.zhang <xuan.zhang@okg.com>
2022-03-29 23:02:14 +0800

    feat: 向顺序表指定位置插入元素

--- sequence-list/index.c
+++ sequence-list/index.c
@@ -28,10 +28,44 @@ table initTable() {
 }
 // 顺序打印顺序表中元素
 void printTable(table t) {
-    for(int i = 0; i < Size i++) {
+    for(int i = 0; i < t.length; i++) {
         printf("%d\n", t.head[i]);
     }
 }
+/**
+ * 插入元素
+ * 1、插入表头
+ * 2、插入中间
+ * 3、插入表尾
+ * 将要插入元素的位置以及后续元素向后移动一位
+ * 放入元素
+ * @return 更新的表
+ */
+table insertItem(table t, int item, int index) {
+    // 插入的位置不存在或者插入的位置比t.length + 1还大
+    if (index < 0 || index > t.length + 1) {
+        printf("插入的位置有问题\n");
+        return t;
+    }
+    // 表没有多余空间，要先申请
+    if (t.length == t.size) {
+        t.head = (int*) realloc(t.head, (t.size + 1) * sizeof(int));
+        if (!t.head) {
+            printf("申请空间失败");
+            return t;
+        }
+        t.size += 1;
+    }
+    // 将元素后移
+    for (int i = t.length - 1; i >= index - 1; i--) {
+        t.head[i + 1] = t.head[i];
+    }
+    // 将元素插入
+    t.head[index - 1] = item;
+    t.length++;
+    return t;
+}
+
 int main() {
     int i;
     table t = initTable();
@@ -41,6 +75,8 @@ int main() {
         t.length++;
     }
     printf("顺序表中存储的元素是：\n");
-    printTable(t);
+    // 向顺序表插入元素
+    table insertedTable = insertItem(t, 6, 3);
+    printTable(insertedTable);
     return 0;
 }
