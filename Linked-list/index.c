655f4cbc219937933c4f1fec8b58673772c06f56
xuan.zhang <xuan.zhang@okg.com>
2022-04-10 21:38:01 +0800

    feat: 重写单链表

--- Linked-list/index.c
+++ Linked-list/index.c
@@ -3,46 +3,46 @@
  */
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
- * 定义单链表节点
- * 指针域指向的是下一个节点整体
- * 所以next指针类型也为结构体
- * 即结构体指针，用于指向一个结构体
+ * 定义链表节点
+ * 每个节点都是LinkNode结构体
+ * 指针指向的后继元素也是节点结构体
+ * 故指针的类型为struct LinkNode
  */
 typedef struct LinkNode {
-    int elem;
+    // 数据域
+    int age;
+    // 指针域
     struct LinkNode * next;
 }LinkNode;
 
 /**
-* 初始化链表
-* 定义头指针
-* 创建多个存储数据的节点
-* 创建过程中要随时与前驱节点建立联系
-*/
+ * 初始化链表
+ * 无头节点单链表
+ */
 LinkNode * initLink() {
-    // 定义头指针 指针类型为结构体LinkNode
-    LinkNode * p = NULL;
-    // 定义首元节点 节点类型是LinkNode结构体
-    // malloc申请一个节点空间
-    // firstNode是一个指针，指向的是结构体LinkNode的实例
-    LinkNode * firstNode = (LinkNode *) malloc(sizeof (LinkNode));
+    // 定义头指针
+    LinkNode *p = NULL;
+    // 定义首元节点
+    // 首元节点也是一个指针，用来指向内存随机开辟的一块空间
+    LinkNode *firstNode = (LinkNode*) malloc(sizeof (LinkNode));
     // 初始化首元节点
-    firstNode->elem = 0;
+    firstNode->age = 0;
     firstNode->next = NULL;
     // 头指针指向首元节点
     p = firstNode;
-    // 从第二个节点开始创建
-    for (int i = 1; i < 5; i++) {
-        LinkNode * node = (LinkNode *) malloc(sizeof (LinkNode));
-        node->elem = i;
+    for(int i = 1; i < 5; i++) {
+        // 创建一个新节点并初始化
+        LinkNode * node = (LinkNode*) malloc(sizeof (LinkNode));
+        node->age = i;
         node->next = NULL;
-        // 将首元节点与新建立的节点建立关系
+        // 将首元节点链接上node节点
         firstNode->next = node;
-        // 指针firstNode每次都指向新链表的最后一个节点
+        // 指针firstNode每次都指向新链表的的最后一个节点
         firstNode = firstNode->next;
     }
-    // 返回头指针，通过头指针即可找到整个链表
+    // 返回建立的节点，返回头指针p即可
     return p;
 }
 
@@ -50,18 +50,18 @@ LinkNode * initLink() {
  * 打印链表
  */
 void printLink(LinkNode *p) {
-    // 将temp指针指向头节点
+    // 将temp指针指向头指针p
     LinkNode * temp = p;
-    while (temp) {
-        printf("%d ", temp->elem);
+    while(temp) {
+        printf("%d,", temp->age);
+        // 指针移动
         temp = temp->next;
     }
-    printf("\n");
 }
 
 int main() {
-    LinkNode *p = NULL;
-    printf("初始链表为:\n");
+    LinkNode * p = NULL;
+    printf("初始化链表为：\n");
     p = initLink();
     printLink(p);
     return 0;
