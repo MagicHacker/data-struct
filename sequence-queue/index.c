d4cfe239ac41558211188cdc39c588153382d6f0
MagicHacker <zxmagichacker@gmail.com>
2022-06-28 14:42:52 +0800

    feat: 入队

--- sequence-queue/index.c
+++ sequence-queue/index.c
@@ -5,16 +5,37 @@
 #include <stdlib.h>
 #define Size 5
 
-// 顺序队列结构
-typedef struct Squeue {
-    // 队头指针
-    int top;
-    // 队尾指针
-    int rear;
-    // 队列数组
-    int * data;
-}Squeue;
+// 入队
+int pushQueue(int * a, int rear, int data) {
+    if (rear == Size) {
+        printf("队列已满");
+        exit(0);
+    }
+    a[rear] = data;
+    rear++;
+    return rear;
+}
+// 出队
+void popQueue(int * a, int front, int rear) {
 
+}
+// 打印队列
+void printQueue(int * a, int rear) {
+    for (int i = 0; i < rear; i++) {
+        printf("%d", a[i]);
+    }
+}
 int main() {
+    int data[Size];
+    // 队头指针，队尾指针
+    int front,rear;
+    // 队列为空 队头和队尾指向同一处
+    front = rear = 0;
+    // 入队
+    rear = pushQueue(data, rear, 1);
+    rear = pushQueue(data, rear, 2);
+    rear = pushQueue(data, rear, 3);
+    rear = pushQueue(data, rear, 4);
+    printQueue(data, rear);
     return 0;
 }
