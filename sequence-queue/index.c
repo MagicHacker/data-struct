012eba82e9b637e1623e5fc19a4f8a4f8ff0028f
MagicHacker <zxmagichacker@gmail.com>
2022-06-28 11:03:18 +0800

    feat: 初始化顺序队列

--- sequence-queue/index.c
+++ sequence-queue/index.c
@@ -3,6 +3,17 @@
  */
 #include <stdio.h>
 #include <stdlib.h>
+#define Size 5
+
+// 顺序队列结构
+typedef struct Squeue {
+    // 队头指针
+    int top;
+    // 队尾指针
+    int rear;
+    // 队列数组
+    int * data;
+}Squeue;
 
 int main() {
     return 0;
