6da8264c9fc70537293800b2e777240df7e91d47
MagicHacker <zxmagichacker@gmail.com>
2022-09-28 22:46:43 +0800

    feat: 查找某个结点的双亲结点的值

--- sequence-tree/index.c
+++ sequence-tree/index.c
@@ -17,10 +17,31 @@ typedef ElemType BiTree[NodeNum];
 void initBiTree(BiTree T) {
     ElemType node;
     int i = 0;
+    printf("按照层次从左往右输入树中结点的值，0 表示空结点，# 表示输入结束:");
     while(scanf("%d", &node)) {
         T[i] = node;
         i++;
     }
+}
+
+// 查找某个结点的双亲结点的值
+ElemType Parent(BiTree T, ElemType e) {
+    int i;
+    if (T[0] == 0) {
+        printf("存储的是一颗空树");
+    } else {
+        if (T[0] == e) {
+            printf("当前结点是根结点，没有双亲结点");
+            return 0;
+        }
+        for (i = 1; i < NodeNum; i++) {
+            if (T[i] == e) {
+                return T[(i + 1) / 2 - 1];
+            }
+        }
+        printf("二叉树中没有指定结点\n");
+    }
+    return -1;
 }
 
 int main() {
