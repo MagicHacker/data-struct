66ea7eadaf4824d43626ef2204c66c335aed1f29
MagicHacker <zxmagichacker@gmail.com>
2022-10-02 23:42:44 +0800

    feat: 中序遍历二叉树线索化

--- thread-tree/index.c
+++ thread-tree/index.c
@@ -1,67 +1,51 @@
 /**
  * 线索二叉树
- * 将已有的二叉树线索化变成线索二叉树
  */
 #include <stdio.h>
 #include <stdlib.h>
 
+// 结点数据域类型
 typedef char TElemType;
-// Link = 0表示指向左右子树指针 Thread = 1表示指向前驱后继指针
+// 枚举 Link = 0表示指向左右孩子指针 Thread = 1表示指向前驱或后继的线索
 typedef enum { Link, Thread } PointerTag;
 typedef struct BiThrNode {
     TElemType data;
-    // 左右子树指针
     struct BiThrNode *lchild, *rchild;
+    // 左标志 为0时指向左子树，为1时指向前驱
     PointerTag LTag;
+    // 右标志 为0时指向右子树，为1时指向后继
     PointerTag RTag;
-} BiThrNode, * BiThrTree;
-
-// 临时指针
+} BiThrNode, *BiThrTree;
+/**
+ * 中序遍历初始化线索二叉树
+ * 原理：遍历过程中修改空指针的指向
+ */
+// 临时指针 始终指向刚访问过的结点
 BiThrTree pre;
-// 初始化线索二叉树
-// 中序遍历线索化
 void initThreadTree(BiThrTree T) {
     if (T) {
         // 递归左子树线索化
         initThreadTree(T->lchild);
-        // 没有左子树
+        // 没有左子树 指向前驱
         if (!T->lchild) {
-            // 前驱线索
             T->LTag = Thread;
+            // 指向前驱
             T->lchild = pre;
         }
-        // 前驱没有右子树
+        // 当前结点没有右子树
         if (!pre->rchild) {
-            // 后继线索
             pre->RTag = Thread;
+            // 前驱右子树指向后继
             pre->rchild = T;
         }
+        // pre始终指向T的前驱
         pre = T;
         // 递归右子树线索化
         initThreadTree(T->rchild);
     }
 }
 
-// 中序遍历线索二叉树
-void inOrderThreadTree(BiThrTree T) {
-    // 临时指针
-    BiThrTree p;
-    p = T->lchild;
-    while(!p == T) {
-        while(p->LTag == Link) {
-            p = p->lchild;
-            printf("%c", p->data);
-            while(p->RTag == Thread && p->rchild != T) {
-                p = p->rchild;
-                printf("%c", p->data);
-            }
-        }
-        p = p->rchild;
-    }
-}
 int main() {
-    BiThrTree T = 's';
-    initThreadTree(T);
-    inOrderThreadTree(T);
+    initThreadTree();
     return 0;
 }
