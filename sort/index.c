5dd9c0902d82104c99aac4ce5d14df5585ab8ef8
MagicHacker <zxmagichacker@gmail.com>
2022-10-06 16:24:38 +0800

    feat: 冒泡优化

--- sort/index.c
+++ sort/index.c
@@ -6,7 +6,7 @@
 
 // 冒泡排序
 // 95183742
-void bubbleSort(List *l) {
+void bubbleSort1(List *l) {
     for (int i = 0; i < l->length; i++) {
         for (int j = i + 1; j < l->length; j++) {
             if (l->data[i] > l->data[j]) {
@@ -19,6 +19,19 @@ void bubbleSort(List *l) {
     }
 }
 
+// 冒泡优化
+void bubbleSort2(List *l) {
+    for (int i = 0; i < l->length; i++) {
+        for (int j = l->length - 1; j >= i; j--) {
+            if (l->data[j] > l->data[j+1]) {
+                // 交换
+                int temp = l->data[j];
+                l->data[j] = l->data[j+1];
+                l->data[j+1] = temp;
+            }
+        }
+    }
+}
 int main() {
     return 0;
 }
