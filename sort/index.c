c9e76f6d0d5e0c6d6ccddfecb5313b662d112e1e
MagicHacker <zxmagichacker@gmail.com>
2022-10-06 16:03:25 +0800

    feat: 简单冒泡排序

--- sort/index.c
+++ sort/index.c
@@ -4,6 +4,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// 冒泡排序
+// 95183742
+void bubbleSort(List *l) {
+    for (int i = 0; i < l->length; i++) {
+        for (int j = i + 1; j < l->length; j++) {
+            if (l->data[i] > l->data[j]) {
+                // 交换
+                int temp = l->data[i];
+                l->data[i] = l->data[j];
+                l->data[j] = temp;
+            }
+        }
+    }
+}
 
 int main() {
     return 0;
